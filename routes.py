c60d50d27651c6b1f9268e019a97c45dfa8dc861
Kamilya <kamilyaph@gmail.com>
2021-12-06 18:47:11 -0500

    update comments

--- routes.py
+++ routes.py
@@ -4,10 +4,13 @@ from app import app
 from flask import render_template
 import sqlite3
 
+# array holds the golfers statistics
 golf = []
+
+# array holds the first letter of each golfers name
 letter = []
 
-# route is a URl the application implements.
+# route is the URl the application implements.
 
 # view functions handle the application routes. View functions are mapped to
 #   specific route URLs
@@ -17,7 +20,9 @@ letter = []
 def home():  # put application's code here
     return render_template('home.html')
 
-
+#   route to the players page
+#   the players view function queries the database for each player name and their stats
+#   as well as the first letter of each player
 @app.route('/players')
 def players():
     # create a connection to the database
@@ -42,7 +47,8 @@ def players():
 
     return render_template('players.html', golfers=golf, letter=letter)
 
-
+#   route to the p_gained_lost page
+#   the p_gained_lost view function queries the database for each player name and their stats
 @app.route('/p_gained_lost')
 def gained_lost():
     # create a connection to the database
@@ -63,7 +69,8 @@ def gained_lost():
 
     return render_template('p_gained_lost.html', golfers=json.dumps(golf))
 
-
+#   route to the total_avg page
+#   the total_avg view function queries the database for each player name and their stats
 @app.route('/total_avg')
 def total_avg():
     # create a connection to the database
